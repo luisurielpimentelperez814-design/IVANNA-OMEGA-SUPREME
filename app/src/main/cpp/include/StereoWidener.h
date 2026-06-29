81e12a5f0ae89cb0522fa8aa469edb597e32ed87
luisurielpimentelperez814-design <luisurielpimentelperez814@gmail.com>
2026-06-29 08:52:16 -0600

    Update StereoWidener.h

--- app/src/main/cpp/include/StereoWidener.h
+++ app/src/main/cpp/include/StereoWidener.h
@@ -1,17 +1,11 @@
 #pragma once
-#include "dsp_types.h"
-
 namespace ivanna {
-
 class StereoWidener {
 public:
-    void setParams(const DSPParams& p);
-    void process(float* left, float* right, int frames);
-    void reset();  // FIX: Added missing reset() method
-
+    void setWidth(float w);
+    void process(float* l, float* r, int frames);
 private:
-    float width_     = 1.f;
-    float halfWidth_ = 0.5f;  // width * 0.5, precomputado
+    float width_ = 1.0f;
+    [[maybe_unused]] float halfWidth_ = 0.5f;
 };
-
-} // namespace ivanna
+}
