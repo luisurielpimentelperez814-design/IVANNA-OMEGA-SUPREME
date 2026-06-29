8e2df6daf2d36372551d9304e141cb744685c3b3
luisurielpimentelperez814-design <luisurielpimentelperez814@gmail.com>
2026-06-29 08:19:35 -0600

    Update Compressor.h

--- app/src/main/cpp/include/Compressor.h
+++ app/src/main/cpp/include/Compressor.h
@@ -1,26 +1,22 @@
 #pragma once
-#include "dsp_types.h"
-
-namespace ivanna {
 
 class Compressor {
 public:
-    void setParams(const DSPParams& p);
-    void process(float* left, float* right, int frames);
-    void reset();
+    Compressor();
+    void setThreshold(float db);
+    void setRatio(float ratio);
+    void setAttack(float ms);
+    void setRelease(float ms);
+    float process(float input);
 
 private:
-    float threshold_   = -12.f;
-    float ratio_       = 4.f;
-    float attackCoef_  = 0.f;
-    float releaseCoef_ = 0.f;
-    float makeupGain_  = 1.f;
-    float env_         = 0.f;
-    // Precomputados para evitar resta en el hot loop
-    float inv_atk_     = 1.f;  // 1 - attackCoef_
-    float inv_rel_     = 1.f;  // 1 - releaseCoef_
-    float slope_       = 0.75f; // 1 - 1/ratio_
-    uint32_t sr_       = 48000;
-};
+    float threshold_ = -12.0f;
+    float ratio_ = 4.0f;
+    float attackCoef_ = 0.99f;
+    float releaseCoef_ = 0.999f;
+    float envelope_ = 0.0f;
 
-} // namespace ivanna
+    [[maybe_unused]] float inv_atk_ = 1.f;
+    [[maybe_unused]] float inv_rel_ = 1.f;
+    [[maybe_unused]] float slope_ = 0.75f;
+};
