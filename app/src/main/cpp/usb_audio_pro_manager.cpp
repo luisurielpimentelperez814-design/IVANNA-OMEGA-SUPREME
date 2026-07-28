03bf8f63dd47d3f8716d0ca45f157fd8add41710
luisurielpimentelperez814-design <luisurielpimentelperez814@users.noreply.github.com>
2026-07-28 01:19:33 -0600

    Fix: Declaración de g_engine_running para pipeline de GitHub Actions

--- app/src/main/cpp/usb_audio_pro_manager.cpp
+++ app/src/main/cpp/usb_audio_pro_manager.cpp
@@ -7,6 +7,8 @@
 #include <android/log.h>
 #include <atomic>
 
+extern std::atomic<bool> g_engine_running;
+
 #define LOG_TAG "UsbAudioProManager"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
